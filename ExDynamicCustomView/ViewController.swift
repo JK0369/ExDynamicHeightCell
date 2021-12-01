f50b43aedf14a5d235d2915a54a9acbbb24d2624
jake <palatable7@naver.com>
2021-12-01 23:39:53 +0900

    Fix model

--- ExDynamicCustomView/ViewController.swift
+++ ExDynamicCustomView/ViewController.swift
@@ -18,9 +18,9 @@ class ViewController: UIViewController {
     let disposeBag = DisposeBag()
     
     var sections = [
-        MySection(section: "첫 번째", items: [MyModel(message: "jake"), MyModel(message:"jake의 iOS")]),
-        MySection(section: "두 번째", items: [MyModel(message:"jake의 iOS앱 개발 알아가기"), MyModel(message:"jake의 iOS앱 개발 알아가기\njake의 iOS앱 개발 알아가기")]),
-        SectionModel(section: "세 번째", items: [MyModel(message:"jake의 iOS앱 개발 알아가기\njake의 iOS앱 개발 알아가기\njake의 iOS앱 개발 알아가기\njake의 iOS앱 개발 알아가기")])
+        MySection(model: "첫 번째", items: [MyModel(message: "jake"), MyModel(message:"jake의 iOS")]),
+        MySection(model: "두 번째", items: [MyModel(message:"jake의 iOS앱 개발 알아가기"), MyModel(message:"jake의 iOS앱 개발 알아가기\njake의 iOS앱 개발 알아가기")]),
+        SectionModel(model: "세 번째", items: [MyModel(message:"jake의 iOS앱 개발 알아가기\njake의 iOS앱 개발 알아가기\njake의 iOS앱 개발 알아가기\njake의 iOS앱 개발 알아가기")])
     ]
     
     /// row 데이터 적용 (section은 dataSource.titleForHeaderInSection으로 설정)
@@ -52,7 +52,7 @@ class ViewController: UIViewController {
         sectionSubject.accept(sections)
         
         dataSource.titleForHeaderInSection = { dataSource, index in
-            return dataSource.sectionModels[index].section
+            return dataSource.sectionModels[index].model
         }
     }
     
@@ -67,6 +67,12 @@ class ViewController: UIViewController {
             .bind(to: titleLabel.rx.text)
             .disposed(by: disposeBag)
         
+        Observable
+            .zip(tableView.rx.itemSelected, tableView.rx.modelSelected(MySection.Item.self))
+            .map { "셀 선택 \($0),\n\($1)" }
+            .bind(to: titleLabel.rx.text)
+            .disposed(by: disposeBag)
+        
         tableView.rx.itemDeleted
             .map { "아이템 제거 = \($0)" }
             .bind(to: titleLabel.rx.text)
@@ -76,7 +82,6 @@ class ViewController: UIViewController {
             .map { "아이템 이동 \n= \($0)" }
             .bind(to: titleLabel.rx.text)
             .disposed(by: disposeBag)
-        
     }
     
     // viewDidLoad()에서 호출
